b78b68e74f1968688b0f0048c7511cd6fdae7673
diorkhIoe <despoire.dvrbx@gmail.com>
2021-11-19 18:37:17 -0700

    Update plan.js

--- commands/plan.js
+++ commands/plan.js
@@ -35,6 +35,28 @@ module.exports = {
            .setDescription("Great! Now, what will the departing airport be? Please send a **link.**")
            .setAuthor("Turkish Airlines Administrative A.I.","https://cdn.discordapp.com/attachments/909976331897425941/911410962362429490/turkish-airlines-logo-1E368810A4-seeklogo.com.png")
            message.channel.send(embed2);
+
+           const filter2 = (m) => m.author.id === message.author.id;
+           const collector2 = message.channel.createMessageCollector(filter2, { max: 1, time: 180000 })
+
+           collector2.on('collect', (msg2) => {
+               console.log(msg2.content)
+               var airportlink = msg2.content
+            const embed3 = new Discord.MessageEmbed();
+            embed3
+             .setColor("#c90119")
+             .setTitle("TK Flight Planning")
+             .setDescription("Great! Now, what will the **destination** be? Please write this in __plain text.__")
+             .setAuthor("Turkish Airlines Administrative A.I.","https://cdn.discordapp.com/attachments/909976331897425941/911410962362429490/turkish-airlines-logo-1E368810A4-seeklogo.com.png")
+            message.channel.send(embed3);
+            const filter3 = (m) => m.author.id === message.author.id;
+            const collector3 = message.channel.createMessageCollector(filter3, { max: 1, time: 180000 })
+
+            collector3.on('collect', (msg3) => {
+                console.log(msg3.content)
+            })
+
+           })
         })
 	}
 }
